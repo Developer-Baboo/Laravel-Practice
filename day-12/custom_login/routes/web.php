08645f1579aa477b8ff6933aa5ba9a9b5f2509bc
Developer Baboo <babooheerani@gmail.com>
2023-09-13 17:27:32 +0500

    Laravel Admin And User Login

--- day-12/custom_login/routes/web.php
+++ day-12/custom_login/routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    dd("test");
+    // return view('welcome');
 });
 
 Route::group(['middleware'=>'guest'], function(){
