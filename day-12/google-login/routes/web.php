88328b54c38a58f5ad612cc8c9dbe8f1516bb8f3
Developer Baboo <babooheerani@gmail.com>
2023-09-13 06:39:01 +0500

    developed google auth

--- day-12/google-login/routes/web.php
+++ day-12/google-login/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GoogleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('auth/google', [GoogleController::class, 'loginWithGoogle'])->name('login');
+Route::get('auth/google/callback', [GoogleController::class, 'callbackFromGoogle'])->name('callback');
+
+
+Route::get('home', function () {
+    return view('home');
+})->name('home');
