338c890cf81be65126bcedb576488c658187c961
Developer Baboo <babooheerani@gmail.com>
2023-09-17 17:11:52 +0500

    created chat app

--- day-12(custom login+google Login+multi auth)/google-login/google-auth/app/Http/Controllers/GoogleController.php
+++ day-12(custom login+google Login+multi auth)/google-login/google-auth/app/Http/Controllers/GoogleController.php
@@ -10,47 +10,57 @@ use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
 {
-    public function loginWithGoogle(){
+    // This method redirects the user to Google for authentication.
+    public function loginWithGoogle()
+    {
         return Socialite::driver('google')->redirect();
     }
 
+    // This method handles the callback from Google after authentication.
     public function callbackFromGoogle()
     {
-        //jo user auth hova ha wo kya apna sath la arha ha
-
-        //jo abhi login hova ha google us ka data $user variable man save kriveinga phir wo db man store krvienga]
-
         try
         {
+            // Retrieve user information from Google.
             $user = Socialite::driver('google')->user();
+
+            // Check if a user with the same email already exists in your database.
             $is_user = User::where('email', $user->getEmail())->first();
-            if(!$is_user)
+
+            if (!$is_user)
             {
+                // If the user doesn't exist, create a new user or update the existing one.
                 $saveUser = User::updateOrCreate(
                     [
-                        'google_id' =>$user->getId(),
+                        'google_id' => $user->getId(),
                     ],
                     [
                         'name' => $user->getName(),
                         'email' => $user->getEmail(),
-                        'password' => Hash::make($user->getName().'@'.$user->getId()),
+                        'password' => Hash::make($user->getName().'@'.$user->getId()), // Generating a password based on user data (this may not be secure).
                     ]
-                    );
+                );
             }
             else
             {
+                // If the user already exists, update the 'google_id' in their record.
                 $saveUser = User::where('email', $user->getEmail())->update([
                     'google_id' => $user->getId(),
                 ]);
+
+                // Retrieve the updated user record.
                 $saveUser = User::where('email', $user->getEmail())->first();
             }
+
+            // Log in the user using their ID.
             Auth::loginUsingId($saveUser->id);
 
+            // Redirect the authenticated user to the 'home' route.
             return redirect()->route('home');
         }
         catch (\Throwable $th)
         {
-            throw $th;
+            throw $th; // Handle any exceptions that occur during this process.
         }
     }
 }
