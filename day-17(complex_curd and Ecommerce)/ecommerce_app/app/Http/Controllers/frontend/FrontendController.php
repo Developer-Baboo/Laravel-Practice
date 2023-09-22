70fc39e094b78c85824ebfea48dff28d44a94993
Developer Baboo <babooheerani@gmail.com>
2023-09-22 08:15:44 +0500

    changes

--- day-17(complex_curd and Ecommerce)/ecommerce_app/app/Http/Controllers/frontend/FrontendController.php
+++ day-17(complex_curd and Ecommerce)/ecommerce_app/app/Http/Controllers/frontend/FrontendController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class FrontendController extends Controller
 {
     public function index(){
-        return view('frontend.index');
+        //take 15 Products
+        $featured_products = Product::where('trending', '1')->take(15)->get();
+        return view('frontend.index', compact('featured_products'));
+        // return view('frontend.index');
     }
 }
--- day-17(complex_curd and Ecommerce)/ecommerce_app/resources/views/frontend/index.blade.php
+++ day-17(complex_curd and Ecommerce)/ecommerce_app/resources/views/frontend/index.blade.php
@@ -3,8 +3,24 @@
 @section('title')
     Welcome to E-Shop
 @endsection
-
-
 @section('content')
-<h1>Welcome</h1>
+@include('layouts.inc.slider')
+
+<div class="py-5">
+    <div class="container">
+        <div class="row">
+            @foreach ($featured_products as $prod)
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="{{ asset('assets/uploads/product/'.$prod->image) }}" alt="Product Image">
+                    <div class="card-body">
+                        <h5> Name : {{ $prod->name }}</h5>
+                        <small>Price : {{ $prod->selling_price }}</small>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
 @endsection
