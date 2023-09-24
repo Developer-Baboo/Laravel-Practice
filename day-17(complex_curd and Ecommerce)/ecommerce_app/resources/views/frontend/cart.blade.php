3be253d8cd21c5aa76944bd533bf33d5c6385347
Developer Baboo <babooheerani@gmail.com>
2023-09-24 23:43:09 +0500

    calcuate total price of cart

--- day-17(complex_curd and Ecommerce)/ecommerce_app/resources/views/frontend/cart.blade.php
+++ day-17(complex_curd and Ecommerce)/ecommerce_app/resources/views/frontend/cart.blade.php
@@ -19,17 +19,21 @@
 <div class="container my-3">
     <div class="card shadow">
         <div class="card-body">
+            @php $total = 0;  @endphp
             @foreach ($cartitems as $item )
                 <div class="row product_data">
-                    <div class="col-md-2">
+                    <div class="col-md-2 my-auto">
                          {{-- //below products is the name of functions defined in cart model --}}
                         <img src=" {{ asset('assets/uploads/product/'.$item->products->image) }} " height="50px" width="50px"  alt="Image Here">
                     </div>
-                    <div class="col-md-5">
+                    <div class="col-md-3 my-auto">
                         {{-- //below products is the name of functions defined in cart model --}}
                         <h6> {{ $item->products->name }} </h6>
                     </div>
-                    <div class="col-md-3">
+                    <div class="col-md-2 my-auto">
+                        <h6>{{ $item->products->selling_price }}</h6>
+                    </div>
+                    <div class="col-md-3 my-auto">
                         <input type="hidden" class="prod_id" value=" {{$item->prod_id}} " >
                         <label for="Quantity">Quantity</label>
                         <div class="input-group text-center mb-3" style="width:130px;">
@@ -42,7 +46,11 @@
                         <button class="btn btn-danger delete-cart-item"> <i class="fa fa-trash"></i> Remove</button>
                     </div>
                 </div>
+                @php $total += $item->products->selling_price * $item->prod_qty; @endphp
             @endforeach
+        </div>
+        <div class="card-footer">
+            <h6>Total Price : {{ $total }} </h6>
         </div>
     </div>
 </div>
