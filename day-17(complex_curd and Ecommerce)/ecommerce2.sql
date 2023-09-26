fd8050c88f603b9eb490d803c0675aa92acd0a19
Developer Baboo <babooheerani@gmail.com>
2023-09-26 19:46:39 +0500

    ..

--- day-17(complex_curd and Ecommerce)/ecommerce2.sql
+++ day-17(complex_curd and Ecommerce)/ecommerce2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 26, 2023 at 05:48 AM
+-- Generation Time: Sep 26, 2023 at 04:45 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -133,6 +133,7 @@ CREATE TABLE `orders` (
   `status` tinyint(4) NOT NULL DEFAULT 0,
   `message` varchar(255) DEFAULT NULL,
   `tracking_no` varchar(255) NOT NULL,
+  `total_price` varchar(1111) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -141,13 +142,9 @@ CREATE TABLE `orders` (
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `user_id`, `fname`, `lname`, `email`, `phone`, `address1`, `address2`, `city`, `state`, `country`, `pincode`, `status`, `message`, `tracking_no`, `created_at`, `updated_at`) VALUES
-(1, '', 'baboo', 'kumar', 'baboo23@gmail.com', '03422449445', 'address1', 'address2', 'city', 'state', 'pak', '12', 0, NULL, 'Baboo5551', '2023-09-25 17:46:43', '2023-09-25 17:46:43'),
-(2, '', 'Mohan', 'Lal', 'mohan@gmail.com', '03422449445', 'karachi', 'mithi', 'city', 'sate', 'Country', 'Country', 0, NULL, 'Baboo7093', '2023-09-25 17:52:10', '2023-09-25 17:52:10'),
-(3, '', 'baboo', 'kumar', 'baboo23@gmail.com', '03422449445', 'address1', 'address2', 'city', 'state', 'pak', '12', 0, NULL, 'Baboo8615', '2023-09-25 18:25:04', '2023-09-25 18:25:04'),
-(4, '', 'Mohan', 'kumar', 'mohan@gmail.com', '03422449445', 'Address1', 'Address2', 'Jamshoro', 'State', 'Pakistan', 'Pin Code', 0, NULL, 'Baboo5672', '2023-09-25 18:26:46', '2023-09-25 18:26:46'),
-(5, '', 'Mohan', 'kumar', 'mohan@gmail.com', '03422449445', 'Address1', 'Address2', 'Jamshoro', 'State', 'Pakistan', 'Pin Code', 0, NULL, 'Baboo5290', '2023-09-25 18:27:50', '2023-09-25 18:27:50'),
-(6, '', 'Mohan', 'kumar', 'mohan@gmail.com', '03422449445', 'Address1', 'Address2', 'Jamshoro', 'State', 'Pakistan', 'Pin Code', 0, NULL, 'Baboo2234', '2023-09-25 19:04:15', '2023-09-25 19:04:15');
+INSERT INTO `orders` (`id`, `user_id`, `fname`, `lname`, `email`, `phone`, `address1`, `address2`, `city`, `state`, `country`, `pincode`, `status`, `message`, `tracking_no`, `total_price`, `created_at`, `updated_at`) VALUES
+(1, '2', 'Mohan', 'kumar', 'mohan@gmail.com', '03422449445', 'Address1', 'Address2', 'Jamshoro', 'State', 'Pakistan', 'Pin Code', 1, NULL, 'Baboo7909', '80000', '2023-09-26 08:54:36', '2023-09-26 12:23:32'),
+(2, '2', 'Mohan', 'kumar', 'mohan@gmail.com', '03422449445', 'Address1', 'Address2', 'Jamshoro', 'State', 'Pakistan', 'Pin Code', 0, NULL, 'Baboo2448', '33000', '2023-09-26 08:56:32', '2023-09-26 08:56:32');
 
 -- --------------------------------------------------------
 
@@ -170,25 +167,8 @@ CREATE TABLE `order_items` (
 --
 
 INSERT INTO `order_items` (`id`, `order_id`, `prod_id`, `qty`, `price`, `created_at`, `updated_at`) VALUES
-(1, '1', '5', '1', '700', '2023-09-25 17:46:43', '2023-09-25 17:46:43'),
-(2, '1', '4', '2', '1300', '2023-09-25 17:46:43', '2023-09-25 17:46:43'),
-(3, '1', '8', '2', '80000', '2023-09-25 17:46:43', '2023-09-25 17:46:43'),
-(4, '2', '5', '1', '700', '2023-09-25 17:52:10', '2023-09-25 17:52:10'),
-(5, '2', '4', '2', '1300', '2023-09-25 17:52:10', '2023-09-25 17:52:10'),
-(6, '2', '8', '2', '80000', '2023-09-25 17:52:10', '2023-09-25 17:52:10'),
-(7, '3', '5', '1', '700', '2023-09-25 18:25:04', '2023-09-25 18:25:04'),
-(8, '3', '4', '2', '1300', '2023-09-25 18:25:04', '2023-09-25 18:25:04'),
-(9, '3', '8', '2', '80000', '2023-09-25 18:25:04', '2023-09-25 18:25:04'),
-(10, '4', '5', '1', '700', '2023-09-25 18:26:46', '2023-09-25 18:26:46'),
-(11, '4', '4', '2', '1300', '2023-09-25 18:26:46', '2023-09-25 18:26:46'),
-(12, '4', '8', '2', '80000', '2023-09-25 18:26:46', '2023-09-25 18:26:46'),
-(13, '5', '5', '1', '700', '2023-09-25 18:27:50', '2023-09-25 18:27:50'),
-(14, '5', '4', '2', '1300', '2023-09-25 18:27:50', '2023-09-25 18:27:50'),
-(15, '5', '8', '2', '80000', '2023-09-25 18:27:50', '2023-09-25 18:27:50'),
-(16, '6', '5', '1', '700', '2023-09-25 19:04:15', '2023-09-25 19:04:15'),
-(17, '6', '4', '2', '1300', '2023-09-25 19:04:15', '2023-09-25 19:04:15'),
-(18, '6', '8', '2', '80000', '2023-09-25 19:04:15', '2023-09-25 19:04:15'),
-(19, '6', '7', '1', '48000', '2023-09-25 19:04:15', '2023-09-25 19:04:15');
+(1, '1', '1', '1', '80000', '2023-09-26 08:54:36', '2023-09-26 08:54:36'),
+(2, '2', '2', '1', '33000', '2023-09-26 08:56:32', '2023-09-26 08:56:32');
 
 -- --------------------------------------------------------
 
@@ -265,13 +245,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `cate_id`, `name`, `slug`, `small_description`, `description`, `original_price`, `selling_price`, `image`, `qty`, `tax`, `status`, `trending`, `meta_title`, `meta_keywords`, `meta_description`, `created_at`, `updated_at`) VALUES
-(1, 3, 'Samsung Tablet', 'samsung-tablet', 'It is one of the high quality samsung table in pakistan', 'sdfsd', '90000', '80000', '1695295827.png', '33', '33', 1, 1, 'sdf', 'sfsdf', 'sfsd', '2023-09-20 20:30:27', '2023-09-20 20:30:27'),
-(2, 3, 'Iphone Keypad', 'iphone-keypad', 'It is one of the high quality samsung table in pakistan', 'sdfsd', '35000', '33000', '1695295869.png', '3', '3', 0, 1, 'sdfsd', 'sdfsd', 'sdfsd', '2023-09-20 20:31:09', '2023-09-20 20:31:09'),
-(3, 4, 'Baby Dress', 'baby-dress', 'It is one of the high quality baby dress in pakistan', 'Small DescriptionSmall DescriptionSmall Description', '1400', '1300', '1695360527.png', '5', '60', 1, 1, 'meta_title', 'Meta KeyWordsMeta KeyWords', 'Meta DescriptionMeta Description', '2023-09-21 14:28:47', '2023-09-21 14:28:47'),
-(4, 4, '3-Piece', '3-piece', 'It is one of the high quality 3 Piece in pakistan', 'DescriptionDescription', '1400', '1300', '1695360640.png', '30', '10', 1, 1, 'meta_title', 'Meta KeyWordsMeta KeyWords', 'Meta KeyWordsMeta KeyWords', '2023-09-21 14:30:40', '2023-09-21 14:30:40'),
-(5, 4, 'Formal Shirt', 'formal-shirt', 'It is one of the high quality samsung table in pakistan', 'Meta KeyWordsMeta KeyWordsMeta KeyWords', '800', '700', '1695360746.png', '4', '50', 1, 1, 'Meta KeyWords', 'Meta KeyWordsMeta KeyWords', 'Meta KeyWordsMeta KeyWordsMeta KeyWords', '2023-09-21 14:32:26', '2023-09-21 14:32:26'),
-(6, 3, 'Head Set', 'Slug', 'Small DescriptionSmall Description', 'DescriptionDescription', '4000', '3000', '1695360848.jpg', '30', '200', 0, 0, 'meta_title', 'Meta KeyWordMeta KeyWord', 'Meta KeyWordMeta KeyWordMeta KeyWord', '2023-09-21 14:34:08', '2023-09-21 14:34:08'),
-(7, 3, 'Dell Laptop', 'dell-laptop', 'Meta KeyWordMeta KeyWord', 'Meta KeyWordMeta KeyWord', '40000', '48000', '1695360920.jpg', '5', '30', 1, 1, 'Meta KeyWord', 'Meta KeyWordMeta KeyWord', 'Meta KeyWordMeta KeyWord', '2023-09-21 14:35:20', '2023-09-21 14:35:20'),
+(1, 3, 'Samsung Tablet', 'samsung-tablet', 'It is one of the high quality samsung table in pakistan', 'sdfsd', '90000', '80000', '1695295827.png', '1', '33', 1, 1, 'sdf', 'sfsdf', 'sfsd', '2023-09-20 20:30:27', '2023-09-26 08:49:17'),
+(2, 3, 'Iphone Keypad', 'iphone-keypad', 'It is one of the high quality samsung table in pakistan', 'sdfsd', '35000', '33000', '1695295869.png', '1', '3', 0, 1, 'sdfsd', 'sdfsd', 'sdfsd', '2023-09-20 20:31:09', '2023-09-26 08:56:32'),
+(3, 4, 'Baby Dress', 'baby-dress', 'It is one of the high quality baby dress in pakistan', 'Small DescriptionSmall DescriptionSmall Description', '1400', '1300', '1695360527.png', '1', '60', 1, 1, 'meta_title', 'Meta KeyWordsMeta KeyWords', 'Meta DescriptionMeta Description', '2023-09-21 14:28:47', '2023-09-26 04:27:32'),
+(4, 4, '3-Piece', '3-piece', 'It is one of the high quality 3 Piece in pakistan', 'DescriptionDescription', '1400', '1300', '1695360640.png', '4', '10', 1, 1, 'meta_title', 'Meta KeyWordsMeta KeyWords', 'Meta KeyWordsMeta KeyWords', '2023-09-21 14:30:40', '2023-09-26 04:27:32'),
+(5, 4, 'Formal Shirt', 'formal-shirt', 'It is one of the high quality samsung table in pakistan', 'Meta KeyWordsMeta KeyWordsMeta KeyWords', '800', '700', '1695360746.png', '1', '50', 1, 1, 'Meta KeyWords', 'Meta KeyWordsMeta KeyWords', 'Meta KeyWordsMeta KeyWordsMeta KeyWords', '2023-09-21 14:32:26', '2023-09-26 08:31:54'),
+(6, 3, 'Head Set', 'Slug', 'Small DescriptionSmall Description', 'DescriptionDescription', '4000', '3000', '1695360848.jpg', '1', '200', 1, 1, 'meta_title', 'Meta KeyWordMeta KeyWord', 'Meta KeyWordMeta KeyWordMeta KeyWord', '2023-09-21 14:34:08', '2023-09-26 08:31:54'),
+(7, 3, 'Dell Laptop', 'dell-laptop', 'Meta KeyWordMeta KeyWord', 'Meta KeyWordMeta KeyWord', '40000', '48000', '1695360920.jpg', '2', '30', 1, 1, 'Meta KeyWord', 'Meta KeyWordMeta KeyWord', 'Meta KeyWordMeta KeyWord', '2023-09-21 14:35:20', '2023-09-26 04:27:32'),
 (8, 3, 'Lenvo Laptop', 'lenovo-laptop', 'Meta KeyWordMeta KeyWord', 'Meta KeyWordMeta KeyWord', '88000', '80000', '1695360981.jpg', '4', '20', 1, 1, 'Meta KeyWord', 'Meta KeyWordMeta KeyWord', 'Meta KeyWordMeta KeyWord', '2023-09-21 14:36:21', '2023-09-21 14:36:21');
 
 -- --------------------------------------------------------
@@ -306,7 +286,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `lname`, `phone`, `address1`, `address2`, `city`, `state`, `country`, `pincode`, `role_as`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Baboo', 'baboo@gmail.com', NULL, '$2y$10$LMLt325bycj6R7QYVWKLauq4BXjKDpb26cbMh9eotyVPxqS2plz1m', '0', '0', '', '', '', '', '', '', 1, 'i0G8khrijikVfHyAMNX68qB5zEZdBaIrckMDnfytgfaPqz4kBdMZ6ej5v4Tj', '2023-09-19 14:41:56', '2023-09-19 14:41:56'),
-(2, 'Mohan', 'mohan@gmail.com', NULL, '$2y$10$usZH6T3cRQV7awI/iuH6nOm5oW7RyhlPJNmmuNts81rba5FcZ3Ntq', 'kumar', '03422449445', 'Address1', 'Address2', 'Jamshoro', 'State', 'Pakistan', 'Pin Code', 0, 'qAaun3IQprcAG0hU3Moeb69Oj5SlBFquRRSvsqApMzArQrRGk1chxhQvx2Em', '2023-09-19 15:26:04', '2023-09-25 18:27:50');
+(2, 'Mohan', 'mohan@gmail.com', NULL, '$2y$10$usZH6T3cRQV7awI/iuH6nOm5oW7RyhlPJNmmuNts81rba5FcZ3Ntq', 'kumar', '03422449445', 'Address1', 'Address2', 'Jamshoro', 'State', 'Pakistan', 'Pin Code', 0, 'hddeSJI0zYDkEyvoOE61RcXswvsGaB8YiWb315WAfkmXgTinS7HIV0A5XRLB', '2023-09-19 15:26:04', '2023-09-25 18:27:50');
 
 --
 -- Indexes for dumped tables
@@ -390,7 +370,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -414,13 +394,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
