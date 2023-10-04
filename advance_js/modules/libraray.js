65888fc1c0873b4332dfb5a7f39766d607cd95d3
Developer Baboo <babooheerani@gmail.com>
2023-10-04 12:39:25 +0500

    modules

---
+++ advance_js/modules/libraray.js
@@ -0,0 +1,33 @@
+// export default function(){
+//     console.log("Hello");
+// }
+
+//  let name1 = "Baboo kumar";
+
+//  function user(name){
+
+//     // console.log(`Hello ${name}`);
+//     return `Hello ${name}`;
+
+// }
+
+
+
+//  class abc{
+//     constructor(){
+//         console.log("Constructor Method");
+//     }
+// }
+
+
+function user(){
+    return `Hello`
+}
+
+
+
+export {user};
+
+// export {name1, user, abc}; //It is your choice how you write export, write like this one 
+// or write individuals before above names of variable, class and function
+
---
+++ advance_js/modules/main.js
@@ -0,0 +1,28 @@
+// import {default as baboo} from './libraray.js';
+// import {name1, user, abc} from './libraray.js';
+// import baboo from './libraray.js';
+
+import {user} from './bridge.js';
+
+// baboo();
+
+// import {name1 as nm,user as us, abc as ab} from './libraray.js';
+// import * as baboo from './libraray.js'; //important all things, methods,classes, variables and set alias as baboo
+
+// console.log(baboo.name1);
+
+// // document.body.innerHTML = name;
+// // user("Baboo Kumar");
+
+// console.log(baboo.user("Baboo Heerani"));
+
+
+// let a = new baboo.abc();
+
+
+// console.log(name1);
+// console.log(user("Baboo Heerani"));
+// let a = new abc();
+
+
+console.log(user());
